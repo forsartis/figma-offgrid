2eb2963f0966080699c759549c57a9a286d4520f
Jens Eggerstedt <j.eggerstedt@kaibatech.de>
2020-10-12 11:22:12 +0200

    use tailwinds internal purge option

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
-const purgecssConfig = {
-  content: ['./src/**/*.vue'],
-  whitelist: ['html', 'body'],
-  defaultExtractor: content =>
-    content.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [],
-};
-
-const isProduction = process.env.NODE_ENV === 'production';
-
 module.exports = {
   plugins: {
     tailwindcss: './tailwind.config.js',
     autoprefixer: {},
-    '@fullhuman/postcss-purgecss': isProduction ? purgecssConfig : false,
   },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['./src/**/*.vue'],
   theme: {
     fontFamily: {
       inter: [
